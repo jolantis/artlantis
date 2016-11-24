aa6be1463acb7d03e192ef88117c8caca5f92748
Jonathan van Wunnik <jonathan@artlantis.nl>
2016-11-24 16:11:26 +0100

    Add thumb config option for interlace (or progressive) rendering.

--- site/config/config.php
+++ site/config/config.php
@@ -410,6 +410,7 @@ Thumbs
 
 c::set('thumbs.driver', 'im');                                                  // The thumbnail library which is being used by Kirby's thumb function/class ('gd' or 'im')
 c::set('thumbs.quality', 82);                                                   // The default jpg compression (or quality setting) for thumbs (figure plugin and tag);
+c::set('thumbs.interlace', false);                                              // Save thumb to use progressive (interlace line) rendering, meaning the image is delivered and rendered in stages
 c::set('thumbs.icc.profile', true);                                             // Preserve embedded color (or icc) profile in generated thumb only (any other profile and meta-data is removed); only works when thumb driver is set to 'im' (ImageMagick)
 c::set('thumbs.meta.data', false);                                              // Preserve all embedded meta data in generated thumb (e.g. EXIF, IPTC, color profile, etc.); only works when thumb driver is set to 'im' (ImageMagick)
 
