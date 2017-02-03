60f28fbbbde3687e8f37b4e78f3481b0df56f149
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-02-03 09:20:29 +0100

    Add `rel=“me”` to copyright notice.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -8,7 +8,7 @@
 
 	<div role="contentinfo" class="contentinfo">
 		<footer>
-			<?php echo $site->copyright()->kirbytext(); ?>
+			&copy; <?php echo '2002&ndash;' . date("Y"); ?> <a href="https://jonathanvanwunnik.com" rel="me"><?php echo $site->copyright()->smartypants(); ?></a>.
 			<p><small>All contents licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" title="Creative Commons Attribution-Non-Commercial-No-Derivs 4.0 International">CC BY-NC-ND license</a>.</small></p>
 			<p>mobile</p>
 		</footer>
