b934d038fd1c2f638cf7fb9f75cf01f4fedfc0bd
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-02-03 11:58:29 +0100

    Add `crossorigin` to font `rel=“preload”` link.
    To prevent double loading of fonts in Chrome and FF.

--- site/snippets/html-head.php
+++ site/snippets/html-head.php
@@ -64,8 +64,8 @@ $fontobserver = (isset($_COOKIE['fonts-loaded']) && $_COOKIE['fonts-loaded'] == 
 	<link rel="dns-prefetch" href="https://www.google-analytics.com">
 
 	<?php // Preload assets (fonts, stylesheets, etc.) ?>
-	<link rel="preload" href="<?php $site->url(); ?>/assets/fonts/kawak/kawak-bold.woff2" as="font" type="font/woff2">
-	<link rel="preload" href="<?php $site->url(); ?>/assets/fonts/kawak/kawak-light.woff2" as="font" type="font/woff2">
+	<link rel="preload" href="<?php $site->url(); ?>/assets/fonts/kawak/kawak-bold.woff2" as="font" type="font/woff2" crossorigin>
+	<link rel="preload" href="<?php $site->url(); ?>/assets/fonts/kawak/kawak-light.woff2" as="font" type="font/woff2" crossorigin>
 
 	<?php // Page title and (meta) description ?>
 	<?php snippet('page-title-description') ?>
