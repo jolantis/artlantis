965f0d0684a40df36297bafd1f690c258ff2b1f6
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-02-03 09:23:44 +0100

    Minor update to URL/SSL config variables and comments.

--- site/config/config.php
+++ site/config/config.php
@@ -92,21 +92,28 @@ URL / SSL
 
 To use root relative URLs:
 
-c::set('url', '');
+c::set('url', '/');
 
 Or when in a subdirectory:
 
 c::set('url', '/mysite');
 
-To force SSL on every page, make sure to include https
-in your url setup and set the sll setting to true:
+And when set to `false`, Kirby tries to auto-detects the correct URL:
+
+c::set('url', false);
+
+---
+
+To force **SSL** on every page, make sure to include https
+in the url setup and set the **sll** setting to true:
 
 c::set('url', 'https://yourdomain.com');
+c::set('ssl', true);
 
 */
 
-c::set('url', '');
-c::set('ssl', false);
+c::set('url', 'https://artlantis.nl');
+c::set('ssl', true);
 
 
 /* -----------------------------------------------------------------------------
