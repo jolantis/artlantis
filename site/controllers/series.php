c63b51f426a727a939c0eaccf2cbb12fa16906fc
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-08-22 15:12:23 +0200

    FIx series template/controller.

--- site/controllers/series.php
+++ site/controllers/series.php
@@ -1,36 +1,18 @@
 <?php
 return function($site, $pages, $page, $data) {
 
-	// fetch the basic set of pages
+	// set defaults
+	$page_num     = false;
+	$pagination   = false;
+	$filter_key   = false;
+	$filter_value = false;
+
+	// fetch the basic set of pages and define pagination
 	$series = $page->children()->visible()->flip();
+	$pagination = (c::get('pagination.' . $page->intendedTemplate()) == false) ? false : true;
+	$series = ($pagination) ? $page->children()->visible()->flip()->paginate(c::get('pagination.' . $page->intendedTemplate())) : $page->children()->visible()->flip();
 
-	// add the category filter
-	if(count($data) > 0) {
-		$categoryparam = $data;
-		$series = $series->filterBy('categories', $data['category'], ',');
-	}
-	else {
-		$categoryparam = false;
-	}
-
-	// fetch all categories
-	$categories = $site->find('series')->children()->visible()->pluck('categories', ',', true);
-	sort($categories);
-
-	// apply pagination
-	if(isset($data['category']) && !param('page')) {
-		$key = (key($data) == 'tag' ? 'tags' : key($data));
-		$tag = tagunslug($data['category']);
-		// $series = $page->children()->visible()->filterBy($key, ($tag), ',')->flip();
-		$series = $page->children()->visible()->filterBy('categories', ($tag), ',')->flip(); // Set 'categories' as key, because we need to change the content file!
-		$pagination = false;
-	}
-	else {
-		$pagination = (c::get('pagination.' . $page->intendedTemplate()) == false) ? false : true;
-		$series = ($pagination) ? $page->children()->visible()->flip()->paginate(c::get('pagination.' . $page->intendedTemplate())) : $page->children()->visible()->flip();
-	}
-
-	return compact('series', 'categories', 'categoryparam', 'pagination', 'key', 'tag');
+	return compact('series', 'page_num', 'pagination', 'filter_key', 'filter_value');
 
 };
 ?>
