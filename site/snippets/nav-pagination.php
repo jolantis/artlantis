16347fce0d0757bc5f583e5744aa067b89fceca9
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-09-05 13:35:53 +0200

    Style pagination and add svg arrow icons.

--- site/snippets/nav-pagination.php
+++ site/snippets/nav-pagination.php
@@ -7,20 +7,30 @@
 <?php if($pagination && $pagination->hasPages()): ?>
 	<div role="navigation" class="pagination contain-padding">
 		<?php /* <h2 class="is-hidden-visually">Page navigation</h2> */ ?>
-		<ul>
+		<ul class="pagination__list">
 			<?php if($pagination->hasPrevPage()): ?>
 				<li class="pagination__item pagination__item--prev">
 					<?php if($page_num == 2): ?>
-						<a href="<?php echo url(kirby()->request()->path()->first() . (($filter_value) ? '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value : '')); ?>">Previous page</a>
+						<a href="<?php echo url(kirby()->request()->path()->first() . (($filter_value) ? '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value : '')); ?>">
 					<?php else: ?>
-						<a href="<?php echo url(kirby()->request()->path()->first() . (($filter_value) ? '/' . $filter_value : '') . '/page/' . ($page_num - 1)); ?>">Previous page</a>
+						<a href="<?php echo url(kirby()->request()->path()->first() . (($filter_value) ? '/' . $filter_value : '') . '/page/' . ($page_num - 1)); ?>">
 					<?php endif; ?>
+							<svg role="presentation" width="24" height="24" title="Left arrow">
+								<use xlink:href="/assets/images/sprite.svg#arrow-left"/>
+							</svg>
+							Previous page
+						</a>
 				</li>
 			<?php endif; ?>
 
 			<?php if($pagination->hasNextPage()): ?>
 				<li class="pagination__item pagination__item--next">
-					<a href="<?php echo url(kirby()->request()->path()->first() . (($filter_value) ? '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value : '') . '/page/' . ($page_num + 1)); ?>">Next page</a>
+					<a href="<?php echo url(kirby()->request()->path()->first() . (($filter_value) ? '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value : '') . '/page/' . ($page_num + 1)); ?>">
+						Next page
+						<svg role="presentation" width="24" height="24" title="Right arrow">
+							<use xlink:href="/assets/images/sprite.svg#arrow-right"/>
+						</svg>
+					</a>
 				</li>
 			<?php endif; ?>
 		</ul>
