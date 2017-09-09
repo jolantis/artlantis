7d24e574982369ffe1b328182fcc79c5381040b4
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-09-09 15:08:56 +0200

    Comment.

--- assets/javascript/head.scripts.js
+++ assets/javascript/head.scripts.js
@@ -16,6 +16,6 @@ if (!Modernizr.inlinesvg) {
 
 	svg4everybody({
 		nosvg: true, // shiv <svg> and <use> elements and use image fallbacks
-		polyfill: true // polyfill <use> elements for External Content
+		polyfill: true // polyfill <use> elements for external content
 	});
 }
