ba4150bba8019dde8c9152425ca0b9f512301fea
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-09-27 16:07:01 +0200

    Update next-prev navigation markup.

--- site/snippets/nav-prevnext.php
+++ site/snippets/nav-prevnext.php
@@ -11,15 +11,15 @@
 <?php if($prev || $next): ?>
 	<div role="navigation" class="pagination contain-padding">
 		<?php /* <h2 class="is-hidden-visually">Page navigation</h2> */ ?>
-		<ul>
+		<ul class="pagination__list">
 			<?php if($next): ?>
-				<li class="pagination__item pagination__item--next">
-					<?php /* if($filter_value):?>
-						<a href="<?php echo url($next->parent() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value . '/' . $next->uid()); ?>">Newer post <span>(<?php echo $next->title(); ?>)</span></a>
-					<?php else: ?>
-						<a href="<?php echo $next->url(); ?>">Newer post <span>(<?php echo $next->title(); ?>)</span></a>
-					<?php endif; */ ?>
-					<a href="<?php echo $next->url(); ?>">Newer post <span>(<?php echo $next->title(); ?>)</span></a>
+				<li class="pagination__item pagination__item--newer">
+					<a href="<?php echo $next->url(); ?>" class="icon icon--left">
+						<svg role="presentation" width="24" height="24" title="Left arrow">
+							<use xlink:href="/assets/images/sprite.svg#arrow-left"/>
+						</svg>
+						<?php echo $next->title(); ?>
+					</a>
 				</li>
 			<?php endif ?>
 
@@ -30,13 +30,13 @@
 			*/ ?>
 
 			<?php if($prev): ?>
-				<li class="pagination__item pagination__item--prev">
-					<?php /* if($filter_value): ?>
-						<a href="<?php echo url($prev->parent() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value . '/' . $prev->uid()); ?>">Older post <span>(<?php echo $prev->title(); ?>)</span></a>
-					<?php else: ?>
-						<a href="<?php echo $prev->url(); ?>">Older post <span>(<?php echo $prev->title(); ?>)</span></a>
-					<?php endif; */ ?>
-					<a href="<?php echo $prev->url(); ?>">Older post <span>(<?php echo $prev->title(); ?>)</span></a>
+				<li class="pagination__item pagination__item--older">
+					<a href="<?php echo $prev->url(); ?>" class="icon icon--right">
+						<?php echo $prev->title(); ?>
+						<svg role="presentation" width="24" height="24" title="Right arrow">
+							<use xlink:href="/assets/images/sprite.svg#arrow-right"/>
+						</svg>
+					</a>
 				</li>
 			<?php endif ?>
 		</ul>
