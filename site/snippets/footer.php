40bc02ef71faafbaff2fe5aa19e5a683e99c5f96
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-11-11 17:59:03 +0100

    Add paragraph tags.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -8,7 +8,7 @@
 
 	<div role="contentinfo" class="contentinfo contain-padding">
 		<footer>
-			&copy; <?php echo '2002&ndash;' . date("Y"); ?> <a href="https://jonathanvanwunnik.com" rel="me"><?php echo $site->copyright()->smartypants(); ?></a>.
+			<p>&copy; <?php echo '2002&ndash;' . date("Y"); ?> <a href="https://jonathanvanwunnik.com" rel="me"><?php echo $site->copyright()->smartypants(); ?></a>.</p>
 			<p><small>All contents licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" title="Creative Commons Attribution-Non-Commercial-No-Derivs 4.0 International">CC BY-NC-ND license</a>.</small></p>
 		</footer>
 	</div>
