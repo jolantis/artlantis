e01ea786c0fbeb2f20843033a8ec1c5d920bab60
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-12-24 15:23:49 +0100

    Fix issue with `$page` variable.

--- site/snippets/nav-main.php
+++ site/snippets/nav-main.php
@@ -39,12 +39,12 @@ $loc = isset($loc) ? $loc : false;
 
 		<li class="nav-main__item"<?php echo (page() == 'home' && !str::contains(kirby()->request()->path(), 'tag') && !str::contains(kirby()->request()->path(), 'blog')) ? ' aria-current="page"' : ''; ?>><a rel="home" href="<?php echo $site->url(); ?>">Home</a></li>
 
-		<?php foreach($pages->visible()->not() as $page): ?>
-			<?php /* if($filter_key && $filter_value && $page == 'blog' && ($site->url() . '/' . kirby()->request()->path() != site()->errorPage()->url())): */ ?>
-			<?php if($filter_key && $filter_value && $page == 'blog' && ($site->url() . '/' . kirby()->request()->path() != site()->errorPage()->url())): ?>
-				<li class="nav-main__item"<?php echo (str::contains(kirby()->request()->path(), 'tag')) ? ' aria-current="page"' : ''; ?>><a href="<?php echo url($page->url() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value); ?>"><?php echo $page->title()->smartypants(); ?></a></li>
+		<?php foreach($pages->visible()->not() as $page_item): ?>
+			<?php /* if($filter_key && $filter_value && $page_item == 'blog' && ($site->url() . '/' . kirby()->request()->path() != site()->errorPage()->url())): */ ?>
+			<?php if($filter_key && $filter_value && $page_item == 'blog' && ($site->url() . '/' . kirby()->request()->path() != site()->errorPage()->url())): ?>
+				<li class="nav-main__item"<?php echo (str::contains(kirby()->request()->path(), 'tag')) ? ' aria-current="page"' : ''; ?>><a href="<?php echo url($page_item->url() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value); ?>"><?php echo $page_item->title()->smartypants(); ?></a></li>
 			<?php else: ?>
-				<li class="nav-main__item"<?php echo ($page->isOpen() && !kirby()->request()->path()->nth(1)) ? ' aria-current="page"' : ''; ?>><a href="<?php echo $page->url(); ?>"><?php echo $page->title()->smartypants(); ?></a></li>
+				<li class="nav-main__item"<?php echo ($page_item->isOpen() && !kirby()->request()->path()->nth(1)) ? ' aria-current="page"' : ''; ?>><a href="<?php echo $page_item->url(); ?>"><?php echo $page_item->title()->smartypants(); ?></a></li>
 			<?php endif; ?>
 		<?php endforeach; ?>
 
