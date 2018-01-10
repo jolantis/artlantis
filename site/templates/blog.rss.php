8148b4a14cdb12687a9c0f8315104d858e1caefc
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-01-10 08:28:25 +0100

    Set textfield to ‘photos’ for blog rss feed.

--- site/templates/blog.rss.php
+++ site/templates/blog.rss.php
@@ -7,7 +7,7 @@
 // echo page('blog')->children()->visible()->flip()->limit(20)->feed(array(
 echo page('blog')->children()->visible()->flip()->feed(array(
 	'channel'       => 'blog',
-	'textfield'     => 'text',
+	'textfield'     => 'photos',
 	'image'         => false,                                                   // Include image (after excerpt); first available image in folder is used (set to false when images are included in 'kirbytext' field, and excerpt is set to false, to prevent duplicate images!)
 	'excerpt'       => false,
 	'excerptlimit'  => 'words',                                                 // Limit excerpts by the number of 'words' (default) or 'characters'
