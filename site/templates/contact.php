3aff6e0d353eb49409d9d500aefde13f47078047
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-02-09 18:39:17 +0100

    Add vcard snippet.
    And remove it’s contents from the contact template.

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -12,23 +12,6 @@
 			<?php echo $page->intro()->kirbytext(); ?>
 		<?php endif; ?>
 
-		<div vocab="http://schema.org/" typeof="Person" class="vcard">
-			<span property="name" class="is-hidden-visually">Artlantis Photography</span>
-			<span property="name">Jonathan van Wunnik</span>
-			<img src="https://gravatar.com/avatar/f1e6989e062fe42f36cc3894b0468e0f?s=400.jpg" property="image" alt="Avatar of Jonathan van Wunnik" class="is-hidden-visually">
-			<span property="jobTitle" class="is-hidden-visually">Photographer &amp; UX Designer</span>
-			<div property="address" typeof="PostalAddress">
-					<span property="streetAddress" class="is-hidden-visually">Dalgasvej 12</span>
-					<span property="postalCode" class="is-hidden-visually">6623</span>
-					<span property="addressLocality">Vorbasse</span>,
-					<span property="addressCountry">Danmark</span>
-			</div>
-			<span property="telephone">+45 50 69 93 55</span><br>
-			<a href="mailto:hello@jonathanvanwunnik.com" property="email">hello@jonathanvanwunnik.com</a>
-			<a href="https://jonathanvanwunnik.com" property="url" class="is-hidden-visually">jonathanvanwunnik.com</a><br>
-			cvr number <span property="taxID" class="numerals1">DK39028255</span>
-		</div>
-
 		<?php echo $page->text()->kirbytext(); ?>
 
 	</main>
