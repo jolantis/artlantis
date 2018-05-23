1f25ee12f40ce89bdb062e874f4244e6c6d683de
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-05-23 14:00:37 +0200

    Spacing.

--- site/controllers/work.php
+++ site/controllers/work.php
@@ -3,21 +3,21 @@ return function($site, $pages, $page, $args) {
 
 	// Set defaults
 	$page_num     = false;
-	$pagination = (c::get('pagination.' . $page->intendedTemplate()) == false) ? false : true;
+	$pagination   = (c::get('pagination.' . $page->intendedTemplate()) == false) ? false : true;
 
 	// Fetch key-value filter pair
 	$filter_key   = (cookie::exists('filter_key')) ? cookie::get('filter_key', false) : false;
 	$filter_value = (cookie::exists('filter_value')) ? cookie::get('filter_value', false): false;
 
 	// Fetch basic set of pages
-	$page_items   = $page->children()->visible()->flip();
-	$page_items   = ($pagination) ? $page->children()->visible()->flip()->paginate(c::get('pagination.' . $page->intendedTemplate())) : $page->children()->visible()->flip();
+	$page_items   = $page->children()->visible();
+	$page_items   = ($pagination) ? $page->children()->visible()->paginate(c::get('pagination.' . $page->intendedTemplate())) : $page->children()->visible();
 
 	# Exclude future posts (filter by date)
-	$page_items = $page_items->filterBy('date', '<', time());
+	$page_items   = $page_items->filterBy('date', '<', time());
 
 	# Set pagination
-	$pagination = $page_items->pagination();
+	$pagination   = $page_items->pagination();
 
 	return compact('page_items', 'page_num', 'pagination', 'filter_key', 'filter_value');
 
