ab8247d803a1cf67f59f1dcf0ef8f91aab5f5081
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-05-23 14:03:08 +0200

    Fix footer logic, plus add some ARIA Roles.

--- site/snippets/nav-main.php
+++ site/snippets/nav-main.php
@@ -14,16 +14,16 @@ $loc = isset($loc) ? $loc : false;
 			<ul class="nav-main__list aligner aligner--wrap aligner--center">
 	<?php else: ?>
 		<?php if($loc == 'footer'): ?>
-			<div class="nav-main contain-padding">
+			<div role="navigation" class="nav-main contain-padding">
 				<ul class="nav-main__list aligner aligner--wrap">
 		<?php else: ?>
-			<div class="nav-main">
+			<div role="navigation" class="nav-main">
 				<ul class="nav-main__list">
 		<?php endif; ?>
 	<?php endif; ?>
 <?php else: ?>
 	<?php if($loc == 'header'): ?>
-		<div class="nav-main compact-aligner__item--right">
+		<div role="navigation" class="nav-main compact-aligner__item--right">
 			<ul class="nav-main__list aligner aligner--wrap aligner--center">
 	<?php else: ?>
 		<?php if($loc == 'footer'): ?>
@@ -31,20 +31,22 @@ $loc = isset($loc) ? $loc : false;
 				<h2 class="is-hidden-visually">Main navigation</h2>
 				<ul class="nav-main__list aligner aligner--wrap">
 		<?php else: ?>
-			<div class="nav-main">
+			<div role="navigation" class="nav-main">
 				<ul class="nav-main__list">
 		<?php endif; ?>
 	<?php endif; ?>
 <?php endif; ?>
 
-		<li class="nav-main__item"<?php echo (page() == 'home' && !str::contains(kirby()->request()->path(), 'tag') && !str::contains(kirby()->request()->path(), 'blog')) ? ' aria-current="page"' : ''; ?>><a rel="home" href="<?php echo $site->url(); ?>">Home</a></li>
+		<?php if($loc == 'footer'): ?>
+			<li class="nav-main__item"><a rel="home" href="<?php echo $site->url(); ?>"<?php echo (page() == 'home' && !str::contains(kirby()->request()->path(), 'tag') && !str::contains(kirby()->request()->path(), 'blog')) ? ' aria-current="page"' : ''; ?>>Home</a></li>
+		<?php endif; ?>
 
 		<?php foreach($pages->visible()->not() as $page_item): ?>
 			<?php /* if($filter_key && $filter_value && $page_item == 'blog' && ($site->url() . '/' . kirby()->request()->path() != site()->errorPage()->url())): */ ?>
 			<?php if($filter_key && $filter_value && $page_item == 'blog' && ($site->url() . '/' . kirby()->request()->path() != site()->errorPage()->url())): ?>
-				<li class="nav-main__item"<?php echo (str::contains(kirby()->request()->path(), 'tag')) ? ' aria-current="page"' : ''; ?>><a href="<?php echo url($page_item->url() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value); ?>"><?php echo $page_item->title()->smartypants(); ?></a></li>
+				<li class="nav-main__item"><a href="<?php echo url($page_item->url() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . $filter_value); ?>"<?php echo (str::contains(kirby()->request()->path(), 'tag')) ? ' aria-current="page"' : ''; ?>><?php echo $page_item->title()->smartypants(); ?></a></li>
 			<?php else: ?>
-				<li class="nav-main__item"<?php echo ($page_item->isOpen() && !kirby()->request()->path()->nth(1)) ? ' aria-current="page"' : ''; ?>><a href="<?php echo $page_item->url(); ?>"><?php echo $page_item->title()->smartypants(); ?></a></li>
+				<li class="nav-main__item"><a href="<?php echo $page_item->url(); ?>"<?php echo ($page_item->isOpen() && ($page->depth() === 1)) ? ' aria-current="page"' : ''; ?>><?php echo $page_item->title()->smartypants(); ?></a></li>
 			<?php endif; ?>
 		<?php endforeach; ?>
 
