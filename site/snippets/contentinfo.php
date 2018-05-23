9f2c8e6bfb902d57844f8f52749ab718e4fb89bf
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-05-23 14:02:09 +0200

    Make Creative Commons link secure (https)!

--- site/snippets/contentinfo.php
+++ site/snippets/contentinfo.php
@@ -15,6 +15,6 @@
 
 	<footer class="medium-aligner contain-padding">
 		<p>&copy; <?php echo '2002&ndash;' . date("Y"); ?> <a href="https://jonathanvanwunnik.com" rel="me"><?php echo $site->copyright()->smartypants(); ?></a></p>
-		<p class="medium-aligner__item--right">All contents licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" title="Creative Commons Attribution-Non-Commercial-No-Derivs 4.0 International">CC BY-NC-ND license</a></p>
+		<p class="medium-aligner__item--right">All contents licensed under a <a rel="license" href="https://creativecommons.org/licenses/by-nc-nd/4.0/" title="Creative Commons Attribution-Non-Commercial-No-Derivs 4.0 International">CC BY-NC-ND license</a></p>
 	</footer>
 </div>
