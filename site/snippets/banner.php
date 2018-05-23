bf7a706618885f987a443a2562697ecf2d70438f
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-05-23 14:01:22 +0200

    Add smartypants.

--- site/snippets/banner.php
+++ site/snippets/banner.php
@@ -10,7 +10,7 @@
 		<?php echo ($page->isHomePage()) ? '<h1>' : '<a href="' . $site->url() . '" title="Return to the homepage" rel="home">'; ?>
 			<?php
 				echo ($site->short_title()->isNotEmpty()) ? $site->short_title()->smartypants() : $site->title()->smartypants();
-				echo ($site->short_title()->isNotEmpty() && $site->short_title_addendum()->isNotEmpty()) ? '<span class="is-hidden-visually">: ' . $site->short_title_addendum() . '</span>' : '';
+				echo ($site->short_title()->isNotEmpty() && $site->short_title_addendum()->isNotEmpty()) ? '<span class="is-hidden-visually">: ' . $site->short_title_addendum()->smartypants() . '</span>' : '';
 			?>
 		<?php echo ($page->isHomePage()) ? '</h1>' : '</a>'; ?>
 	</div>
