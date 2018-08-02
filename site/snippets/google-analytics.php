6c1a234bab19df9327552541a4c1130878636eb7
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-08-02 17:34:33 +0200

    Fix issue with incorrect URL in Google Analytics script.
    Better research later! Why does it work with my other sites, but not for this one?

--- site/snippets/google-analytics.php
+++ site/snippets/google-analytics.php
@@ -20,7 +20,7 @@ if (c::get('google.analytics') == true && c::get('google.analytics.id') != 'TRAC
 	<script>
 		// Respect user's privacy
 		if ('navigator.doNotTrack' != 'yes' && 'navigator.doNotTrack' != '1' && 'navigator.msDoNotTrack' != '1') {
-			window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','<?php echo c::get("google.analytics.id", "TRACKING ID IS NOT SET"); ?>','<?php echo str::after($site->url(), '://'); ?>');ga('set', 'anonymizeIp', true);ga('set', 'forceSSL', true);ga('send','pageview');
+			window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','<?php echo c::get("google.analytics.id", "TRACKING ID IS NOT SET"); ?>','<?php $tmp = strstr($site->url(), ':'); echo substr($tmp, 3); //echo str::after($site->url(), '://'); ?>');ga('set', 'anonymizeIp', true);ga('set', 'forceSSL', true);ga('send','pageview');
 		}
 	</script>
 	<script src="https://www.google-analytics.com/analytics.js" async defer></script>
